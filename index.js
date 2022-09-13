2bd6b2084608f66a580cf5fefda1770087acf8e8
Harshvardhan Sahay <bhvima@gmail.com>
2022-09-13 15:50:24 -0400

    Ping everyone

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ client.on('interactionCreate', async interaction => {
 					.setStyle('DANGER'),
 			);
 
-		await interaction.reply({ embeds: [ embed ], components: [row] });
+		await interaction.reply('@everyone', { embeds: [ embed ], components: [row] });
 	}
 });
 
